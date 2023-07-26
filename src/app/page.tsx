eeed9f91251c113bcfb7b0d74bd7e07ce0a01d81
hector <hectorrrivera1010@gmail.com>
2023-07-25 22:04:22 -0600

    add user icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
+import { UserButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
     <div>
-        <h1>Hola bienvenido</h1>
+        <h1 className=''>Hola bienvenido</h1>
+        <div className="user-profile">
+          <UserButton></UserButton>
+        </div>
         <Link href="/userprofile">Ir al perfil</Link>
     </div>
   )
